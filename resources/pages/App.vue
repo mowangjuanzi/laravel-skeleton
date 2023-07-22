119f264a8eeb887af56a4248d0b6092b0e0376c2
魔王卷子 <baoguoxiao0538@hotmail.com>
2023-07-23 00:34:25 +0800

    vite 支持 element-plus 并支持自动按需导入

--- resources/pages/App.vue
+++ resources/pages/App.vue
@@ -1,8 +1,13 @@
 <script setup>
+import {ref} from "vue";
+
+const value = ref(new Date());
 </script>
 
 <template>
-    <h1 class="text-slate-400 hover:text-sky-400">hello world</h1>
+    <h1 class="text-slate-400 hover:text-sky-400">hello world</h1>\
+
+    <el-calendar v-model="value"/>
 </template>
 
 <style scoped>
